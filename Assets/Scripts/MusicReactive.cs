acec874064dbdc25f1315fdc226a1d73cf6bcc61
jaden <cep450@nyu.edu>
2021-11-19 18:15:22 -0500

    setting up for stuff moving to the beat

--- Assets/Scripts/MusicReactive.cs
+++ Assets/Scripts/MusicReactive.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeatController : MonoBehaviour
+public class MusicReactive : MonoBehaviour
 {
 
-    
+    public float xScaleMax = 1;
+    public float xScaleMin = 1;
+    public float yScaleMax = 1;
+    public float yScaleMin = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,6 @@ public class BeatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //bounce 2 the music 
     }
 }
