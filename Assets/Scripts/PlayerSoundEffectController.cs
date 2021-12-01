4404aab896ddbc76230f285f958e0b7cdfc3395e
Sawyer Smith <sps471@nyu.edu>
2021-12-01 12:23:33 -0500

    SFX Declarations

--- Assets/Scripts/PlayerSoundEffectController.cs
+++ Assets/Scripts/PlayerSoundEffectController.cs
@@ -6,8 +6,29 @@ public class PlayerSoundEffectController : MonoBehaviour
 {
 
     public AudioSource src_step;
+    public AudioClip sfx_step;
+
     public AudioSource src_blockHigh;
+    public AudioClip sfx_blockHigh;
+
     public AudioSource src_blockLow;
+    public AudioClip sfx_blockLow;
+
+    public AudioSource src_combo;
+    public AudioClip[] sfx_combos;
+
+    public AudioSource src_switchHigh;
+    public AudioClip sfx_switchHigh;
+
+    public AudioSource src_switchLow;
+    public AudioClip sfx_switchLow;
+
+    public AudioSource src_attackHigh;
+    public AudioClip sfx_attackHigh;
+
+    public AudioSource src_attackLow;
+    public AudioClip sfx_attackLow;
+
 
 
     // Start is called before the first frame update
@@ -23,12 +44,17 @@ public class PlayerSoundEffectController : MonoBehaviour
     }
 
     public void Sfx_Step() {
-
+        src_step.Play();
     }
 
     //aka clash
-    public void Sfx_Block() {
+    public void Sfx_BlockHigh() {
+        src_blockHigh.Play();
+    }
 
+    public void Sfx_BlockLow()
+    {
+        src_blockLow.Play();
     }
 
     public void Sfx_Combo(int num) {
