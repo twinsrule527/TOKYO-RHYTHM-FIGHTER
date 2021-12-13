1f8050c7d73159b84ffad58cd119d5936e96c4a1
jaden <cep450@nyu.edu>
2021-12-12 22:05:48 -0500

    tried and failed to juice score ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,6 +13,15 @@ public class UIManager : MonoBehaviour
 
     public Image p1Renderer;
     public Image p2Renderer;
+    public Image p1juice;
+    public Image p2juice;
+
+    public float startScale;
+    float endScale;
+    public float startOpacity;
+    public float endOpacity;
+
+    public float timescale;
 
     public Sprite [] p1Numbers;
     public Sprite [] p2Numbers;
@@ -22,6 +31,9 @@ public class UIManager : MonoBehaviour
 
     public Character p1;
     public Character p2;
+
+    Vector3 startScaleV, endScaleV;
+    Color startC, endC;
 
     //p1.hitsScored
     //p1.points
@@ -35,6 +47,14 @@ public class UIManager : MonoBehaviour
     {
         //scoreText1.text = " " + 0;
         //scoreText2.text = " " + 0;
+
+        endScale = p1Renderer.transform.localScale.x;
+
+        startScaleV = new Vector3(startScale, startScale, 1);
+        endScaleV = new Vector3(endScale, endScale, 1);
+
+        startC = new Color(1, 1, 1, startOpacity);
+        endC = new Color(1, 1, 1, endOpacity);
     }
 
     // Update is called once per frame
@@ -44,20 +64,34 @@ public class UIManager : MonoBehaviour
         if(p1score != p1.hitsScored) {
             p1score = p1.hitsScored;
             p1Renderer.sprite = p1Numbers[p1score];
+            //p1juice.sprite = p1Numbers[p1score];
+            //StartCoroutine(scoreIncreaseJuice(p1juice));
         }
         if(p2score != p2.hitsScored) {
             p2score = p2.hitsScored;
             p2Renderer.sprite = p2Numbers[p2score];
+            //p2juice.sprite = p2Numbers[p2score];
+            //StartCoroutine(scoreIncreaseJuice(p2juice));
         }
         //scoreText1.text = " " + p1.hitsScored;
         //scoreText2.text = " " + p2.hitsScored;
     }
 
-    IEnumerator scoreIncreaseGrow(Image renderer) {
+    //why doesnt this look right :-( 
+    IEnumerator scoreIncreaseJuice(Image renderer) {
 
-        //TODO make the score number grow or something 
+        renderer.gameObject.SetActive(true);
 
-        yield return null;
+        renderer.transform.localScale = startScaleV;
+        renderer.color = startC;
+
+        for(float tracker = 0; renderer.transform.localScale.x - 0.01f > endScale; tracker += Time.deltaTime * timescale) {
+            renderer.transform.localScale = Vector3.Lerp(startScaleV, endScaleV, tracker);
+            renderer.color = Color.Lerp(startC, endC, tracker);
+            yield return null;
+        }
+        renderer.gameObject.SetActive(false);
+        
     }
 
 }
