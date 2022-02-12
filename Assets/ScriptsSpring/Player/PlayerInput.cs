db7bb386b53761c3d20d67f560c72ef1e67c9031
jaden <cep450@nyu.edu>
2022-02-12 16:12:23 -0500

    organization

---
+++ Assets/ScriptsSpring/Player/PlayerInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+    Takes input from the keyboard and tells other things to do stuff. 
+
+*/
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
