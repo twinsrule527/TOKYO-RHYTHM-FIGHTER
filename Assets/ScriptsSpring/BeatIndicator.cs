2db200f9797a86aeca500eb012686145b6d3b86e
jaden <cep450@nyu.edu>
2022-02-16 12:42:43 -0500

    beat indicator

--- Assets/ScriptsSpring/BeatIndicator.cs
+++ Assets/ScriptsSpring/BeatIndicator.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class BeatIndicator : MonoBehaviour
 {
 
-    [SerializeField] static float total;
+    //THE TOTAL HIGHEST FOR ALL
+    //NEEDS TO BE UPDATED IF ADDING MORE BEATS 
+    static float total = 3f;
 
     int sign;
+    float initial;
 
     // Start is called before the first frame update
     void Start()
     {
-        sign = (int)Mathf.Sign(transform.localPosition.x);
+        initial = transform.localPosition.x;
+        sign = (int)Mathf.Sign(initial);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float newx = Mathf.Abs(transform.localPosition.x);
-        newx += BeatController.beat;
-        newx = sign * Mathf.Repeat(newx, total);
+        float newx = BeatController.beat % total + total;
+        newx = Mathf.Repeat(newx + initial, total);
+        newx *= sign;
+        newx = newx + (total * -sign);
         transform.localPosition = new Vector3(newx, transform.localPosition.y, transform.localPosition.z);
     }
 }
