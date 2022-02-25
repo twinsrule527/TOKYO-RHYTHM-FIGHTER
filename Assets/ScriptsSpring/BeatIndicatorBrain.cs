07ea48a30f2427f90b7adf4dc4a1b3c5a541b587
Eli <eliclendenning@gmail.com>
2022-02-25 18:07:05 -0500

    Start of beat indicator work

---
+++ Assets/ScriptsSpring/BeatIndicatorBrain.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Shows the beat indicator wherever it is on the screen
+    //Tells which beats to be present from boss attacks & telegraphing
+public class BeatIndicatorBrain : MonoBehaviour
+{
+    [SerializeField] private int beatsInAdvanceShown;//How much in advance beats appear on the beat indicator
+
+    //Information that the BeatIndicatorBrain needs to give to a beat indicator:
+        //How long (in beat-form) from now does that note actually occur
+        //what type of beat it is
+    
+
+
+}
