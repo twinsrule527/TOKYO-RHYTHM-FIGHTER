c331001a50560a4334b9a475a87ccff1d41546fd
Eric Ni <en1116@nyu.edu>
2022-02-28 13:51:18 -0500

    Boss animation controller
    starting the animation controller.

---
+++ Assets/ScriptsSpring/Animation/BossSpriteController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpriteController : MonoBehaviour
+{
+
+    public AnimationController shortAttack;
+    public AnimationController longAttack;
+    public AnimationController hurt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
