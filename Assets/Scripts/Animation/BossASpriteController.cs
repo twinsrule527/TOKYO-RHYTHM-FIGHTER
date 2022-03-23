0caf63da3f76ce6c77c6b9733fadaf477e3b1fd1
Eric Ni <en1116@nyu.edu>
2022-03-23 12:48:55 -0400

    Prefab edits for animation
    Affected prefab for boss and player

--- Assets/Scripts/Animation/BossASpriteController.cs
+++ Assets/Scripts/Animation/BossASpriteController.cs
@@ -9,17 +9,28 @@ public class BossASpriteController : BossSpriteController
     public AnimationController longAttack;
     public AnimationController hurt;
 
+    public AnimationController waitAttack1;
+    public AnimationController waitAttack2;
+    public AnimationController length1Attack;
+    public AnimationController length3Attack;
+    public AnimationController oneBeatAttack;
 
-    private void Awake()
+
+     void Awake()
     {
-        shortAttack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
-        longAttack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
-        hurt.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        shortAttack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        longAttack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        hurt.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        waitAttack1.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        waitAttack2.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        length1Attack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        length3Attack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
+        oneBeatAttack.spriteRenderer = gameObject.GetComponentInParent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -42,6 +53,26 @@ public class BossASpriteController : BossSpriteController
             ShortAttack(beatFraction);
         }
         else if(ATKname == "LongAttack") {
+            LongAttack();
+        }
+        else if (ATKname == "WaitAttack1")
+        {
+            LongAttack();
+        }
+        else if (ATKname == "WaitAttack2")
+        {
+            LongAttack();
+        }
+        else if (ATKname == "OneBeatAttack")
+        {
+            LongAttack();
+        }
+        else if (ATKname == "Length1Attack")
+        {
+            LongAttack();
+        }
+        else if (ATKname == "Length3Attack")
+        {
             LongAttack();
         }
         /*else if(ATKname == "WaitAttack") {
@@ -69,4 +100,38 @@ public class BossASpriteController : BossSpriteController
         hurt.spriteRenderer.sprite = hurt.Sprites[0];
         hurt.PlayAnimation();
     }
+
+    public void WaitAttack1()
+    {
+        waitAttack1.spriteRenderer.sprite = waitAttack1.Sprites[0];
+        waitAttack1.PlayAnimation();
+    }
+
+
+    public void WaitAttack2()
+    {
+        waitAttack2.spriteRenderer.sprite = waitAttack2.Sprites[0];
+        waitAttack2.PlayAnimation();
+    }
+
+    public void Length1Attack()
+    {
+        length1Attack.spriteRenderer.sprite = length1Attack.Sprites[0];
+        length3Attack.PlayAnimation();
+    }
+    public void Length3Attack()
+    {
+        length3Attack.spriteRenderer.sprite = length3Attack.Sprites[0];
+        length3Attack.PlayAnimation();//Beat(beatFraction);
+    }
+
+
+    //Mess Up plays from 0 bc it should play immediately when players mess up. 
+    public void OneBeatAttack()
+    {
+        oneBeatAttack.spriteRenderer.sprite = oneBeatAttack.Sprites[0];
+        oneBeatAttack.PlayAnimation();
+    }
+
+    
 }
