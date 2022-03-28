5af718af60d1afcc5c567629f7a1ab932905329f
jaden <cep450@nyu.edu>
2022-03-28 12:47:06 -0400

    shadow clone fix in Shake

--- Assets/Scripts/BeatIndicatorStuff/BossBeatIndicator.cs
+++ Assets/Scripts/BeatIndicatorStuff/BossBeatIndicator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 //A beat indicator used for the bossBeat - might end up being consolidated into the general beat indicator
 public class BossBeatIndicator : BeatIndicator
 {
+
+    //[SerializeField] 
     
     void Awake() {
         startPos = BeatIndicatorBrain.BossIndicatorStartPos;
@@ -13,6 +15,12 @@ public class BossBeatIndicator : BeatIndicator
     public override void SetIndicatorStart(beatIndicatorInfo info) {
         mySprite.sprite = info.indicatorSprite;
         base.SetIndicatorStart(info);
+    }
+
+    public override void PastCenter() {
+        moving = false;
+        enabled = false;
+        mySprite.enabled = false;
     }
     
 }
--- Assets/Scripts/Boss/LevelEditor/AttackGenerator.cs
+++ Assets/Scripts/Boss/LevelEditor/AttackGenerator.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class AttackGenerator : MonoBehaviour
 {
+
+    //dictionary pairing BossAttack files to characters
+
+    //TODO: HOW TO PUT SCRIPTS IN THIS DICTIONARY IN INSPECTOR. 
+    //ACTUALLY, PREFABS ARE IN THE BOSS PREFAB. 
+    
+
+    [SerializeField] char [] symbols;
+    [SerializeField] BossAttack [] attacks;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(symbols.Length != attacks.Length) {
+            Debug.Log("ERROR!!! list of symbols is of different length than list of attacks in level generator dictionary");
+        }
     }
 
     // Update is called once per frame
