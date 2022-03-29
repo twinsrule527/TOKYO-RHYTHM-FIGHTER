400e6c568b4fc50d105f770104b96757473ef98e
jaden <cep450@nyu.edu>
2022-03-28 23:58:52 -0400

    letterbox hp working, though hp doesnt seem to be?

--- Assets/Scripts/Juice/ScaleByHealthPlayer.cs
+++ Assets/Scripts/Juice/ScaleByHealthPlayer.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ScaleByHealthPlayer : MonoBehaviour
 {
 
-    [SerializeField] float scaleFullX, scaleFullY, scaleFullZ, scaleDeadX, scaleDeadY, scaleDeadZ;
+    //TODO: differences or hard values?
+    //right now only transform, cause writing this for the letterbox and its scale is for bounce
+    //[SerializeField] float scaleFullX, scaleFullY, scaleDeadX, scaleDeadY;
     [SerializeField] float transFullX, transFullY, transFullZ, transDeadX, transDeadY, transDeadZ;
 
     // Start is called before the first frame update
@@ -15,12 +17,13 @@ public class ScaleByHealthPlayer : MonoBehaviour
         //hm. come to think of it these could have been vector3s maybe. oh well 
 
         //if empty, initialize full to starting scale 
+        /*
         if(scaleFullX == 0)
             scaleFullX = transform.localScale.x;
         if(scaleFullY == 0)
             scaleFullY = transform.localScale.y;
         if(scaleFullZ == 0)
-            scaleFullZ = transform.localScale.z;
+            scaleFullZ = transform.localScale.z; */
         if(transFullX == 0)
             transFullX = transform.position.x;
         if(transFullY == 0) 
@@ -28,29 +31,41 @@ public class ScaleByHealthPlayer : MonoBehaviour
         if(transFullZ == 0)
             transFullZ = transform.position.z;
 
-        if(scaleDeadX == 0)
+       /* if(scaleDeadX == 0)
             scaleDeadX = transform.localScale.x;
         if(scaleDeadY == 0)
             scaleDeadY = transform.localScale.y;
         if(scaleDeadZ == 0)
-            scaleDeadZ = transform.localScale.z;
+            scaleDeadZ = transform.localScale.z;*/
         if(transDeadX == 0)
             transDeadX = transform.position.x;
         if(transDeadY == 0) 
             transDeadY = transform.position.y;
         if(transDeadZ == 0)
             transDeadZ = transform.position.z;
+        
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        //TODO could make this number only change when player health is changed by calling a func in that place. 
+        //TODO: make this only happen when the health changes. 
+        float healthPercent = Global.Player.playerHealth / Global.Player.playerStartHealth;
+        if(healthPercent == float.NaN) {
+            //if we divided by zero 
+            healthPercent = 0;
+        }
+        float vx = Mathf.Lerp(transDeadX, transFullX, healthPercent);
+        float vy = Mathf.Lerp(transDeadY, transFullY, healthPercent);
+        float vz = Mathf.Lerp(transDeadZ, transFullZ, healthPercent);
+        transform.position = new Vector3(vx, vy, vz);
         
-        //TODO it's lerp min and max hp 
 
-        //TODO MAKE THIS move the bars based on transform actually, since scale is bounce 
-        //
+        //TODO coroutine to lerp into a new value, which can get interrupted by subsequent dmg
+        //or a target value? 
+
+        //TODO: should this hit a threshold like it hits the edges of the indicator near zero?
+        //like youre in the red zone 
+
     }
 }
