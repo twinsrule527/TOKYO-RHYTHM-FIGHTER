873087d981c96a35c242047a118e51bc9b9fbeb5
jaden <cep450@nyu.edu>
2022-04-04 11:52:18 -0400

    fixed 2d array of attack patterns not displaying in inspector

--- Assets/BossHitsPlayerScript.cs
+++ Assets/BossHitsPlayerScript.cs
@@ -7,7 +7,7 @@ public class BossHitsPlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Shake.ShakeTransform(Camera.main.transform, 10, .2f, 50));
+        StartCoroutine(Shake.ShakeTransform(Camera.main.transform, 10, 0.2f, 50));
     }
 
     // Update is called once per frame
--- Assets/Scripts/Boss/LevelEditor/AttackReader.cs
+++ Assets/Scripts/Boss/LevelEditor/AttackReader.cs
@@ -5,7 +5,6 @@ using System;
 
 public class AttackReader : MonoBehaviour
 {
-
     //this is attached to a boss AI prefab.
 
     //RULES FOR TEXT:
@@ -14,7 +13,14 @@ public class AttackReader : MonoBehaviour
 
     //the attack patterns for this boss.
     //[x][y] is the yth file corresponding to the xth boss stage.
-    [SerializeField] TextAsset [][] patternsText;
+    [SerializeField] TextAsset [] stage0, stage1, stage2; //add more stages if needed 
+    TextAsset [][] patternsText;
+
+    void Awake() {
+        patternsText[0] = stage0;
+        patternsText[1] = stage1;
+        patternsText[2] = stage2;
+    }
 
     //pass in the stage number (starting from 0) of attacks to get.
     public List<List<char>> GetPatterns(int stage)
@@ -46,6 +52,7 @@ public class AttackReader : MonoBehaviour
                 linesFromFile.Add(s);
             }
         }
+        
         //for each line
         foreach (string line in linesFromFile) {
 
@@ -78,11 +85,10 @@ public class AttackReader : MonoBehaviour
         }
 
         return patterns;
-
-        //TODO- should this object also disable or destroy itself when done, for efficiency?
-        
     }
 
+    //TODO- should this object also disable or destroy itself when done, for efficiency?
+    
     //temporary for debugging
     //TODO delete later
     /*
