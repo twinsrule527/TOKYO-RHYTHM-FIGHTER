8b52bc6e10a082224870262107a12fff3287b98c
Eric Ni <en1116@nyu.edu>
2022-04-28 20:36:11 -0400

    playtesting chnages

--- Assets/Scripts/Tutorial/TutorialButton.cs
+++ Assets/Scripts/Tutorial/TutorialButton.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class TutorialButton : MonoBehaviour
 {
-
+    private bool timer = false;
+    public int time;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) ) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             Global.TutorialManager.NextStage();
-            gameObject.SetActive(false);
+            timer = true;
+        }
+
+
+
+        if (timer == true)
+        {
+
+            time++;
+            if (time > 100)
+            {
+                gameObject.SetActive(false);
+            }
+
+
         }
     }
 }
---
+++ Assets/Scripts/UI/ProgressableBubbleSPACE.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressableBubbleSPACE : ObjectButton
+{
+
+    [SerializeField] GameObject nextObject;
+    [SerializeField] bool progressOnSpaceAnywhere = false;
+    [SerializeField] bool hideOnSpace = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        if(progressOnSpaceAnywhere || hideOnSpace) {
+            keys.Add(KeyCode.Space);
+        }
+    }
+
+    public void Update()
+    {
+        
+
+    }
+
+    protected override void ButtonPressed()
+    {
+        if(nextObject != null) {
+            nextObject.SetActive(true);
+            Debug.Log("Bruh" + nextObject.ToString());
+        }
+
+        if(functionToCall != null) {
+            base.ButtonPressed();
+        }
+        
+        if(hideOnSpace) {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
