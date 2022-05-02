0d6136be19fc3280d62f16c5d2ef26822df76268
Eric Ni <en1116@nyu.edu>
2022-05-02 13:11:18 -0400

    canvas + hold obj creation
    yes

---
+++ Assets/Scripts/UI/HoldObj.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.identity;
+        //transform.rotation = GetComponentInParent<Transform>().rotation;
+    }
+
+    void OnEnable()
+    {
+        gameObject.GetComponent<Shake>().ShakeIt(2f, 0.2f, 50f);
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
