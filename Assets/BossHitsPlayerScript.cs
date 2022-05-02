5a43a48f799d8570b17c76f307a0996b3baba295
Eric Ni <en1116@nyu.edu>
2022-05-02 13:35:51 -0400

    centerHealth

--- Assets/BossHitsPlayerScript.cs
+++ Assets/BossHitsPlayerScript.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossHitsPlayerScript : MonoBehaviour
+public class BossHitsPlayerScript : CenterEffectAnim
 {
 
-    void OnEnable() {
-        //IF GETTING AN ERROR HERE, PUT A SHAKE SCRIPT ON THE CAMERA! 
-        Debug.Log("tried to shake cam");
-        Camera.main.gameObject.GetComponent<Shake>().ShakeIt(2f, 0.2f, 50f);
-        //StartCoroutine(Shake.ShakeTransform(Camera.main.transform, 10, 0.2f, 50));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
