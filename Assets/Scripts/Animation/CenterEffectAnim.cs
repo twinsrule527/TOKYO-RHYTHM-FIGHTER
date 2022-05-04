d186ae88753b4e731a5b7f43b5aa755458f9fa2a
Eric Ni <en1116@nyu.edu>
2022-05-04 12:10:57 -0400

    AnimationCenter effects test

--- Assets/Scripts/Animation/CenterEffectAnim.cs
+++ Assets/Scripts/Animation/CenterEffectAnim.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 public class CenterEffectAnim : MonoBehaviour
 {
     [SerializeField] List<Sprite> effectList;
-    private SpriteRenderer renderer;
+    [SerializeField] SpriteRenderer rendererCur;
 
-    void OnEnable()
+    public virtual void OnEnable()
     {
         
-        Camera.main.gameObject.GetComponent<Shake>().ShakeIt(2f, 0.2f, 50f);
+        Camera.main.gameObject.GetComponent<Shake>().ShakeIt(1.5f, 0.2f, 50f);
         PlayAnimation();
     }
 
-    private void Start()
+    public virtual void Start()
     {
-        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlayAnimation();
+        }
     }
 
-    public void PlayAnimation()
+    public virtual void PlayAnimation()
     {
         for (int i = 0; i < effectList.Count; i++)
         {
-            renderer.sprite = effectList[i];
+            rendererCur.sprite = effectList[i];
         }
     }
 }
